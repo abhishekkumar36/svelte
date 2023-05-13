a72a00221b3e61ad46e8d4cd6d27b452f0942b17
Abhishek Kumar <flashtoak@gmail.com>
2023-05-13 18:25:30 +0530

    commited

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,44 @@
 <script>
-	let firstName="Abhishek";
-	let lastName="Kumar"
-	$:fullName = `${firstName} ${lastName}`
-	// $:console.log(beltColor)
-	$:{
-		// console.log("Hurray")
-		console.log(firstName);
-		console.log(lastName)
-	}
-	let beltColor = "black";
-	const handleClick = () => {
-		beltColor = "orange";
-	}
-	const handleInput = (e) => {
-		beltColor = e.target.value
-	}
+  let people = [
+    { name: "Rahul", beltColor: "black", age: 25, id: 1 },
+    { name: "Abhishek", beltColor: "yellow", age: 25, id: 2 },
+    { name: "Gunjan", beltColor: "brown", age: 25, id: 3 },
+  ];
+  const handleClick = (id) => {
+    people = people.filter((person) => person.id != id);
+  };
 </script>
 
 <main>
-	<h1>{fullName}</h1>
-	<h1 style="color:{beltColor}">{beltColor}   belt </h1>
-	<!-- <button on:click={handleClick}>update color</button> -->
-	<!-- <input type="text" value={beltColor} on:input={handleInput}> -->
-	<input type="text" bind:value={firstName}>
-	<input type="text" bind:value={lastName}>
-
-	<input type="text" bind:value={beltColor}>
-	</main>
+  {#each people as person (person.id)}
+    <div>
+      <h4>{person.name}</h4>
+      <p>{person.age} years old, {person.beltColor} belt</p>
+      <button on:click={() => handleClick(person.id)}>delete</button>
+    </div>
+  {:else}
+    <p>There is nothing to show</p>
+  {/each}
+</main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
